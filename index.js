5b0155ecef7fe974d28d2e9a23dd6fb1fef4c624
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-11-06 10:10:34 -0400

    Aula01

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const  url  = require("url");
+const app = http.createServer((req,res)=>{
+    // Todas as requisições que chegaem
+    // Vão ser tratradas nessa função
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html;charset=utf8");
+    const urlInfo = url.parse(req.url, true);
+
+    if(urlInfo.pathname == "/usuario"){
+        res.end("Olá Usuário, João");
+    }
+    else if(urlInfo.pathname == "/"){
+        res.end("Página Home");
+    }
+    else if(urlInfo.pathname == "/marvel"){
+        res.end("O universo MCU é melhor que DC!");
+    }
+});
+
+app.listen(8000, (err)=> {
+    console.log("Servidor rodando")
+})

8f26da678f747b3c34bb89951d31836969e7aedf
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-12-13 19:32:56 -0400

    aula get e post

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 // Modulo para trabalhar com caminhos
 const path = require("path");
 const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 // Criar rotas
 // post ele vai cripitografar a informção exemplo tenhoo uma senha ele vai a deixar cripitografada, com get e visivel a informação,não enviamos informações sensiveis com o get
 app.get("/", (req,res)=>{
@@ -12,6 +14,10 @@ app.get("/", (req,res)=>{
 app.get("/login", (req,res) => {
     const caminho = path.join(__dirname, "views","login.html")
     res.sendFile(caminho)
+})
+
+app.post("/login", (req,res) => {
+    res.send("Realizando Login com " +  req.body.email)
 })
 
 app.listen(8000,(err)=>{

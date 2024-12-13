9f36e12dcbddb387d7d43ba551ef9a80c37d5b4b
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-12-13 10:53:00 -0400

    aula 4, trabalhando com formulários

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 // Extra nodemon e um modulo para a reiniciar o servidor automatico, para a execução do projeto utilizamos o comando npm start.
 const express = require("express");
-
+// Modulo para trabalhar com caminhos
+const path = require("path");
 const app = express();
-
 // Criar rotas
 // post ele vai cripitografar a informção exemplo tenhoo uma senha ele vai a deixar cripitografada, com get e visivel a informação,não enviamos informações sensiveis com o get
 app.get("/", (req,res)=>{
-    res.send("Você acessou a raiz do projeto");
+    res.send("Bem-Vindo ao nosso Sistema  <a href='/login'>Entrar</a>");
 });
 
-app.get("/user", (req,res)=>{
-    res.send("Você acessou a rota de usuários");
-});
-
-app.get("/user/:nome", (req,res)=>{
-    let nome = req.params.nome;
-    res.send(`Olá ${nome}`);
-});
+app.get("/login", (req,res) => {
+    const caminho = path.join(__dirname, "views","login.html")
+    res.sendFile(caminho)
+})
 
 app.listen(8000,(err)=>{
     console.log("Server Rodando");
